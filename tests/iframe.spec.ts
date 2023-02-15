a6211f967a01dffebc1628ec18dd7de56acb5163
duyendaodtt <duyen.dao@informa.com>
2023-02-15 15:11:35 +0700

    interaction with frame and inner frame

--- tests/iframe.spec.ts
+++ tests/iframe.spec.ts
@@ -1,13 +1,43 @@
-import {test} from "@playwright/test";
+import {test, expect} from "@playwright/test";
 
 test("example with iframe",async ({page}) => {
     await page.goto('https://letcode.in/frame');
     const allFrames = page.frames();
-    console.log(`Count of frames in this page: ${allFrames}`)
-    const parentFrame = page.frame('firstFr');
-    await parentFrame?.fill("fname", "Test1" );
-    await parentFrame?.fill("lname", "Last name");
+    let frameNo = allFrames.length;
+    console.log(`Count of frames in this page: ${frameNo}`)
+    const parentFrame = page.frame('firstFr'); //using frame name
+    await parentFrame?.fill("input[name='fname']", "Test1" );
+    await parentFrame?.fill("input[name='lname']", "Last name");
 
-    const emailFrame = page.frame('googlefcPresent');
-    await emailFrame?.fill("email", 'test123@offline.com');
+    // const emailFrame = page.frame('googlefcPresent');
+    
+    //failed so only run first expected
+        // expect(await parentFrame?.locator("p.has-text-info").textContent()).toContain("You have entered Test2")
+        // expect(await parentFrame?.locator("p.has-text-info").textContent()).toContain("You have entered Test1")
+    //fail on expect 2 so still run first one
+    expect(await parentFrame?.locator("p.has-text-info").textContent()).toContain("You have entered Test1")
+    expect(await parentFrame?.locator("p.has-text-info").textContent()).toContain("You have entered Test2")
+})
+test('using id frame', async({page}) =>{
+    await page.goto('https://letcode.in/frame');
+    const allFrames = page.frames();
+    let frameNo = allFrames.length;
+    console.log(`Count of frames in this page: ${frameNo}`)
+    const parentFrame = page.frameLocator('#firstFr')
+    await parentFrame.locator("input[name='fname']" ).fill( "Test1");
+    await parentFrame.locator("input[name='lname']" ).fill( "Test2");
+
+    expect(await parentFrame?.locator("p.has-text-info").textContent()).toContain("You have entered Test1")
+})
+
+test.only('inner iFrame', async({page}) =>{
+    await page.goto('https://letcode.in/frame');
+    const allFrames = page.frames();
+    let frameNo = allFrames.length;
+    console.log(`Count of frames in this page: ${frameNo}`)
+    const parentFrame = page.frameLocator('#firstFr')
+    // const innerFrame = page.frameLocator("iframe[src='innerFrame']");
+    const emailFrame = parentFrame.frameLocator("iframe[src='innerFrame']"); 
+    await emailFrame.locator("input[name='email']").fill('test123@offline.com');
+
 })
