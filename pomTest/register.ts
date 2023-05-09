492ddeb08c02b3d8ec20f5ded8005124c4cf1f27
duyendaodtt <duyen.dao@informa.com>
2023-05-09 15:54:28 +0700

    update register.ts file

--- pomTest/register.ts
+++ pomTest/register.ts
@@ -3,12 +3,8 @@ import RegisterPage from "../POM/registerPage";
 
 test("Register TC_01", async({page ,baseURL})=>{
     const registerAccount = new RegisterPage(page); 
-<<<<<<< HEAD:pomTest/register.ts
     await page.goto(`${baseURL}signup`)
-    await registerAccount.submitFullRegisterForm('Ngo', )
-=======
+    await registerAccount.submitFullRegisterForm('Ngo', 'sss', 'emailss', 'VN', '12345', '12345');
     await page.goto(`${baseURL}`)
     await registerAccount.submitFullRegisterForm('Ngo', 'Quyen', 'ngo.q@vietnam.com','Vietnam', 'Pw@12345', 'Pw2345')
-
->>>>>>> 4aba4d86c0d78741a2cd2ed8b6be251a3814a266:pomTest/register.test.ts
 })
